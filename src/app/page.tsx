955e0c452272ccfa6df8f1565ee84613f0aa09da
Gustavo Xavier Saldanha <gustavosaldxav@gmail.com>
2024-03-23 14:17:50 -0300

    Correção da estrutura do código

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,11 @@
-/*
-"use client";
+import Main from '../components/Main'
+import Navbar from '../components/Navbar'
 
-import { useState } from "react";
-import { useRouter } from "next/router";
-import axios from "axios";
-*/
-import Home from "./home";
-
-
-export default function Page() {
-  /*const [nome, setNome] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [carteiraHash, setCarteiraHash] = useState<string>("");
-
-  const handleCadastro = async () => {
-    try {
-      await axios.post("http://localhost:3000/clientes", {
-        nome,
-        email,
-        carteiraHash,
-      });
-
-      //router.push('/dashboard');
-    } catch (error) {
-      console.error("Erro ao cadastrar cliente:", error);
-    }
-  };
-   */
-
+export default function Home() {
   return (
     <div>
-    <Home />
-  </div>
-  );
+    <Navbar />
+    <Main />
+    </div>
+  )
 }
